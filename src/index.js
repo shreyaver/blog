41a956f0ecd3c26bb54d09902826741189e456c8
Shreya Verma <svshreya97@gmail.com>
2019-02-12 09:10:31 +0530

    Added routes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './Components/App';
+import CreatePost from './Components/CreatePost';
 import cardDataArr from './mockData';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-ReactDOM.render(<App cardDataArr= {cardDataArr} />, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+  <div>
+<Route exact path="/home" render={() => (<App cardDataArr = {cardDataArr}/>)}/>
+<Route exact path="/post" component = {CreatePost}/>
+  </div>
+</Router>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
