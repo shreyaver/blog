a04dda72de5425cfa5d7555da12b44c008ee0ffe
Shreya Verma <svshreya97@gmail.com>
2019-02-12 08:14:04 +0530

    Created button component

---
+++ src/Components/Button/Button.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Button from '.';
+import { shallow } from 'enzyme'
+
+describe('Button', () => {
+  it('renders without crashing', () => {
+    
+    const button = renderer.create(<Button caption = 'CREATE POSTS' />).toJSON();
+    expect(button).toMatchSnapshot();
+  });
+  it('sets button caption to passed value', () => {
+    
+    const wrapper = shallow(<Button caption = 'CREATE POSTS' onClick={jest.fn()}/>);
+    expect(wrapper.find('.Button').text()).toEqual('CREATE POSTS');
+  });
+  it('calls passed function on click', () => {
+    const mockFunction = jest.fn();
+    const wrapper = shallow(<Button caption = 'CREATE POSTS' onClick={mockFunction}/>);
+    wrapper.find('.Button').simulate('click');
+    expect(mockFunction).toHaveBeenCalled();
+  });
+});
